03bfb6fa9cd888b8b15f05cea355f49d8d0112f0
MarceloR05 <casillasmarcelo2005@gmail.com>
2025-10-28 23:26:46 -0600

    Update database.js
    actulización

--- server/config/database.js
+++ server/config/database.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'todo_list',
-  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'todoapp',
+  password: process.env.DB_PASSWORD || 'catolica10',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
 });
 
